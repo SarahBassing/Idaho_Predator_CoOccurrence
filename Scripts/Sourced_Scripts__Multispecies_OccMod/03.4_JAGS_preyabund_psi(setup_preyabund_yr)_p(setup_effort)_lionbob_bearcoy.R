00c30da958683d5a9f109f4f8345d3d38bfccf1a
SarahBassing <sarah.bassing@gmail.com>
2024-10-25 15:23:18 -0600

    renamed script and updated parameters

--- Scripts/Sourced_Scripts__Multispecies_OccMod/03.4_JAGS_preyabund_psi(setup_preyabund_yr)_p(setup_effort)_lionbob_bearcoy.R
+++ Scripts/Sourced_Scripts__Multispecies_OccMod/03.4_JAGS_preyabund_psi(setup_preyabund_yr)_p(setup_effort)_lionbob_bearcoy.R
@@ -11,7 +11,7 @@
   #'  Assumes species occur and are detected independently of one another.
   #'  ------------------------------------
   
-  cat(file = './Outputs/03.4_JAGS_preyabund_psi(setup_preyabund_yr)_p(setup_effort)_lionbob.txt', "
+  cat(file = './Outputs/03.4_JAGS_preyabund_psi(setup_preyabund_yr)_p(setup_effort)_lionbob_bearcoy.txt', "
       model{
           
         #### Define Priors  ####
@@ -24,7 +24,7 @@
             
         #'  First order occupancy slopes (psi)
         #'  NOTE: the covariates assigned to betaSpp1 & betaSpp2 will differ!
-        for(fo_psi in 2:7){                         
+        for(fo_psi in 2:8){                         
           betaSpp1[fo_psi] ~ dnorm(0, 0.1)
           betaSpp2[fo_psi] ~ dnorm(0, 0.1)
         }
@@ -114,10 +114,10 @@
           #'  Linear models for the occupancy parameters on the logit scale
               
           #'  ...for states Spp1, Spp2
-          #'  Covariate indices: Spp1 = Intercept[1] + Setup[2] + Year[5] + Elevation[3] + Forest[4] + Elk[7] + White-tailed deer[9]
-          #'  Covariate indices: Spp2 = Intercept[1] + Setup[2] + Year[5] + Elevation[3] + Forest[4] + White-tailed deer[9] + Lagomorph[10]
-          psiSpp1[i] <- betaSpp1[1]*psi_cov[i,1] + betaSpp1[2]*psi_cov[i,2] + betaSpp1[3]*psi_cov[i,5] + betaSpp1[4]*psi_cov[i,3] + betaSpp1[5]*psi_cov[i,4] + betaSpp1[6]*psi_cov[i,7] + betaSpp1[7]*psi_cov[i,9]
-          psiSpp2[i] <- betaSpp2[1]*psi_cov[i,1] + betaSpp2[2]*psi_cov[i,2] + betaSpp2[3]*psi_cov[i,5] + betaSpp2[4]*psi_cov[i,3] + betaSpp2[5]*psi_cov[i,4] + betaSpp2[6]*psi_cov[i,9] + betaSpp2[7]*psi_cov[i,10]
+          #'  Covariate order: Spp1 = Intercept[1] + Setup[2] + Year[3] + Forest[4] + Elevation[5] + TRI[10] + Elk[6] + White-tailed deer[8] 
+          #'  Covariate order: Spp2 = Intercept[1] + Setup[2] + Year[3] + Forest[4] + Elevation[5] + TRI[10] + White-tailed deer[8] + Lagomorph[9] 
+          psiSpp1[i] <- betaSpp1[1]*psi_cov[i,1] + betaSpp1[2]*psi_cov[i,2] + betaSpp1[3]*psi_cov[i,3] + betaSpp1[4]*psi_cov[i,4] + betaSpp1[5]*psi_cov[i,5] + betaSpp1[6]*psi_cov[i,10] + betaSpp1[7]*psi_cov[i,6] + betaSpp1[8]*psi_cov[i,8] 
+          psiSpp2[i] <- betaSpp2[1]*psi_cov[i,1] + betaSpp2[2]*psi_cov[i,2] + betaSpp2[3]*psi_cov[i,3] + betaSpp2[4]*psi_cov[i,4] + betaSpp2[5]*psi_cov[i,5] + betaSpp2[6]*psi_cov[i,10] + betaSpp2[7]*psi_cov[i,8] + betaSpp2[8]*psi_cov[i,9]
         
           #'  ...for state Spp12
           #'  Don't forget - second order parameter set to 0 so no interaction
